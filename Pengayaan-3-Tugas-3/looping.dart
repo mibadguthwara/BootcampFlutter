09240aa87254f0ef2bba5552323b5175bb52ed67
Ibad <mibadguthwara@gmail.com>
2024-06-21 09:38:12 +0700

    update

--- Pengayaan-3-Tugas-3/looping.dart
+++ Pengayaan-3-Tugas-3/looping.dart
@@ -1,17 +1,15 @@
 void main(List<String> args) {
-  // soal2A();
-  soal2B();
+  soal2A();
+  // soal2B();
 }
 
 soal2A() {
-  print('Angka 1 sampai 10');
   for (var i = 1; i <= 10; i++) {
-    print(i);
+    print("angka berurutan ke-$i");
   }
   print("");
-  print("Angka 10 sampai 1");
   for (var j = 10; j >= 1; j--) {
-    print(j);
+    print("Angka Terbalik ke-$j");
   }
 }
 
